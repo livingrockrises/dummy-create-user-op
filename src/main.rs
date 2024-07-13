62a78fbde69c2b6790c8143ac016cffb594ca19c
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2024-07-14 01:16:20 +0530

    init

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,45 @@ sol!(
         uint256 maxPriorityFeePerGas;
         bytes32 paymasterAndData;
     }
+
+    // Review bytes32 vs bytes
+    struct NewUserOperation {
+        address sender;
+        uint256 nonce;
+        address factory; // optionally undefined
+        bytes32 factoryData; // optionally undefined
+        bytes32 callData;
+        uint256 callGasLimit;
+        uint256 verificationGasLimit;
+        uint256 preVerificationGas;
+        uint256 maxFeePerGas;
+        uint256 maxPriorityFeePerGas;
+        address paymaster; // optionally undefined
+        uint256 paymasterVerificationGasLimit; // optionally undefined
+        uint256 paymasterPostOpGasLimit; // optionally undefined
+        bytes32 paymasterData; // optionally undefined
+        // signature ?
+    }
+
+    struct PackedUserOperation {
+        address sender;
+        uint256 nonce;
+        bytes initCode;
+        bytes callData;
+        bytes32 accountGasLimits;
+        uint256 preVerificationGas;
+        bytes32 gasFees;
+        bytes paymasterAndData;
+        bytes signature;
+    }
 );
+
+// need another function to pack user operation.
 
 fn main() {
     let entry_point_version = 6;
-    let entry_point_address = "0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789";
+    let entry_point_address_v6 = "0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789";
+    let entry_point_address_v7 = "0x0000000071727De22E5E9d8BAf0edAc6f37da032";
     let chain_id = 80002;
 
     let user_op_v6 = UserOperation {
@@ -38,11 +72,13 @@ fn main() {
         paymasterAndData: keccak256(hex::decode("0x").unwrap()),
     };
 
+    // let user_op_v7 = //
+
     if entry_point_version == 6 {
         let user_op_hash = keccak256(&user_op_v6.abi_encode());
 
         // pack (userOpHash, entryPointAddress, chainId)
-        let ep_address_padded = format!("{:0>64}", entry_point_address.trim_start_matches("0x"));
+        let ep_address_padded = format!("{:0>64}", entry_point_address_v6.trim_start_matches("0x"));
         let chain_id_hex = format!("{:x}", chain_id);
         let chain_id_padded = format!("{:0>64}", chain_id_hex);
         let concatenated = format!("{}{}{}", user_op_hash, ep_address_padded, chain_id_padded);
@@ -52,7 +88,7 @@ fn main() {
         println!("Hash: {:?}", hash);
     } else if entry_point_version == 7 {
         // let user_op_hash = keccak256(&user_op_v6.abi_encode());
-        // let ep_address_padded = format!("{:0>64}", entry_point_address.trim_start_matches("0x"));
+        // let ep_address_padded = format!("{:0>64}", entry_point_address_v6.trim_start_matches("0x"));
         // let chain_id_hex = format!("{:x}", chain_id);
         // let chain_id_padded = format!("{:0>64}", chain_id_hex);
         // let concatenated = format!("{}{}{}", user_op_hash, ep_address_padded, chain_id_padded);
